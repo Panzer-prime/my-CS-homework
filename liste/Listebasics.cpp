c43c94b77ad6fafb62735ee37b53c2524c7cb682
Panzer <119689044+gg-codermachine-gg@users.noreply.github.com>
2024-04-04 10:02:04 +0300

    Update Listebasics.cpp

--- liste/Listebasics.cpp
+++ liste/Listebasics.cpp
@@ -3,11 +3,13 @@
 #include"lista.h"
 using namespace std;
 
-void inserareDupaUnElement(nod* &cap, nod* elem, int x) {
+void inserareDupaUnElement(nod* &cap, nod* elem, int x)
+{
     nod* p = new nod;
     p->nr = x;
     p->urm = NULL;
-    if (cap == NULL) {
+    if (cap == NULL)
+    {
         cap = p;
         return;
     }
@@ -15,11 +17,14 @@ void inserareDupaUnElement(nod* &cap, nod* elem, int x) {
     elem->urm = p;
 }
 
-void cautareDupaPozitie(nod* cap, int poz) {
+void cautareDupaPozitie(nod* cap, int poz)
+{
     nod* p = cap;
     int i = 0;
-    while (p) {
-        if (i == poz) {
+    while (p)
+    {
+        if (i == poz)
+        {
             cout << "elementul de pe pozitia " << i << " este : " << p->nr;
             break;
         }
@@ -28,11 +33,14 @@ void cautareDupaPozitie(nod* cap, int poz) {
     }
 }
 
-void cautareDupaVal(nod* cap, int val) {
+void cautareDupaVal(nod* cap, int val)
+{
     nod* p = cap;
     int i = 0;
-    while (p) {
-        if (p->nr == val) {
+    while (p)
+    {
+        if (p->nr == val)
+        {
             cout << "elementul cu valuare: " << val << " este aflat pe pozitia " << i;
             break;
         }
@@ -41,15 +49,18 @@ void cautareDupaVal(nod* cap, int val) {
     }
 }
 
-void stergereaDupaPozitie(nod* &cap, int poz) {
+void stergereaDupaPozitie(nod* &cap, int poz)
+{
     nod* temp = cap;
-    if (poz == 0) {
+    if (poz == 0)
+    {
         cap = cap->urm;
         delete temp;
         return;
     }
     temp = cap;
-    for (int i = 0; i < poz - 1; i++) {
+    for (int i = 0; i < poz - 1; i++)
+    {
         temp = temp->urm;
     }
     nod* deSters = temp->urm;
@@ -57,10 +68,13 @@ void stergereaDupaPozitie(nod* &cap, int poz) {
     delete deSters;
 }
 
-void stergeDupaVal(nod* &cap, int val) {
+void stergeDupaVal(nod* &cap, int val)
+{
     nod* temp = cap;
-    while (temp != NULL && temp->urm != NULL) {
-        if (temp->urm->nr == val) {
+    while (temp != NULL && temp->urm != NULL)
+    {
+        if (temp->urm->nr == val)
+        {
             nod* deSters = temp->urm;
             temp->urm = deSters->urm;
             delete deSters;
@@ -69,32 +83,39 @@ void stergeDupaVal(nod* &cap, int val) {
     }
 }
 
-int main() {
+int main()
+{
     nod* cap = NULL;
-    int valoareNoua,pozitieCautata , valoareCautata, pozitieDeSters, valoareDeSters;
+    int valoareNoua, pozitieCautata, valoareCautata, pozitieDeSters, valoareDeSters;
     creare(cap);
     afisare(cap);
-    cout<<"introdu valuare noua: "; cin>>valoareNoua;
-    nod* element = cap->urm;
+    cout << "introdu valuare noua: ";
+    cin >> valoareNoua;
+    nod* element = cap;
     inserareDupaUnElement(cap, element, valoareNoua);
 
-    cout<<"ce pozitie va fi cautata: "; cin>>pozitieCautata;
+    cout << "ce pozitie va fi cautata: ";
+    cin >> pozitieCautata;
     cautareDupaPozitie(cap, pozitieCautata);
 
-    cout<<"ce valoare va fi cautata: "; cin>>valoareCautata;
+    cout << "ce valoare va fi cautata: ";
+    cin >> valoareCautata;
     cautareDupaVal(cap, valoareCautata);
-    cout<<"introdu valoare pe care o vom sterge: "; cin>>pozitieDeSters;
-    cout<<"\nafisarea inaite de stergere ";
+
+    cout << "introdu pozitia de sters: ";
+    cin >> pozitieDeSters;
+    cout << "\nafisarea inainte de stergere: ";
     afisare(cap);
-    cout<<"\nafisarea dupa stergere ";
+    cout << "\nafisarea dupa stergere: ";
     stergereaDupaPozitie(cap, pozitieDeSters);
     afisare(cap);
-    cout<<"Introdu valoare pe care o vom sterge "; cin>>valoareDeSters;
-    cout<<"\nafisarea inaite de stergere";
-    afisare(cap);
-    cout<<"\nstergerea dupa valoare ";
-    stergeDupaVal(cap, valoareDeSters);
 
+    cout << "Introdu valoarea de sters: ";
+    cin >> valoareDeSters;
+    cout << "\nafisarea inainte de stergere: ";
+    afisare(cap);
+    cout << "\nstergerea dupa valoare: ";
+    stergeDupaVal(cap, valoareDeSters);
     afisare(cap);
 
     return 0;
